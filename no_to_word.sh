e7470a1783861de64901e27150b19799de6d5d42
10011999 <motewaraditya77@gmail.com>
2022-05-22 23:51:08 +0530

    added: single digit no in word

---
+++ no_to_word.sh
@@ -0,0 +1,34 @@
+echo "enter a single no"
+read a
+if (( $a == 0 ))
+then 
+     echo "Zero"
+elif (( $a == 1 ))
+then echo "One"
+elif (( $a == 2 ))
+then
+    echo "Two"
+elif (( $a == 3 ))
+then
+    echo "Three"
+elif (( $a == 4 ))
+then
+    echo "Four"
+elif (( $a == 5 ))
+then
+    echo "Five"
+elif (( $a == 6 ))
+then
+    echo "Six"
+elif (( $a == 7 ))
+then
+    echo "Seven"
+elif (( $a == 8 ))
+then
+    echo "Eight"
+elif (( $a == 9 ))
+then
+    echo "Nine"
+else
+    echo "not single digit no"
+fi
