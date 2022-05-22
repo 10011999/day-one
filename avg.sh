10ad8bcdb7d13beba943ea54462e5073ca208b06
10011999 <motewaraditya77@gmail.com>
2022-05-22 11:29:48 +0530

    added: 5 random 2 digit add an avg

---
+++ avg.sh
@@ -0,0 +1,13 @@
+read -p "Enter first  Number:"  a
+read -p "Enter second Number:"  b
+read -p "Enter third Number:"  c
+read -p "Enter fourth Number:"  d
+read -p "Enter fifth Number:"  e
+
+echo $(( 10+$RANDOM%100 ))
+
+sum=$(( $a+$b+$c+$d+$e ))
+echo "Sum is: $sum"
+
+Average=$(( $sum/5 ))
+echo "Average is:$Average"
